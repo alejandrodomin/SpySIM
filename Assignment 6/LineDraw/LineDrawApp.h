15a6fb28d85c3fd8e280c30ec34dde57314fe6ac
A283le <alexdominguez169@gmail.com>
2018-03-05 13:39:43 -0500

    Assignment 6

---
+++ Assignment 6/LineDraw/LineDrawApp.h
@@ -0,0 +1,21 @@
+/***************************************************************
+ * Name:      LineDrawApp.h
+ * Purpose:   Defines Application Class
+ * Author:    Alejandro Dominguez (adomi120@fiu.edu)
+ * Created:   2018-02-14
+ * Copyright: Alejandro Dominguez ()
+ * License:
+ **************************************************************/
+
+#ifndef LINEDRAWAPP_H
+#define LINEDRAWAPP_H
+
+#include <wx/app.h>
+
+class LineDrawApp : public wxApp
+{
+    public:
+        virtual bool OnInit();
+};
+
+#endif // LINEDRAWAPP_H
